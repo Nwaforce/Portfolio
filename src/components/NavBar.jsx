9156c3dabe02e45fa8e51e938da38cee36155e41
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-01-04 05:08:07 +0100

    update my socials

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,15 @@ const NavBar = () => {
             <img src={logo} alt="logo" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaGithub />
+            {/* <FaGithub /> */}
+            <a
+        href="https://github.com/Nwaforce"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="hover:text-gray-600"
+      >
+        <FaGithub />
+      </a>
             <FaLinkedin />
             <FaSquareTwitter />
             <FaInstagram />
