5c502fcbc75b02d73f9fed8a79e7c61f92fc13a7
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-01-09 10:02:54 +0100

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import About from "./components/About";
 const App = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
@@ -9,6 +10,7 @@ const App = () => {
         <div className="container mx-auto px-8">
             <NavBar />
             <Hero />
+            <About /> 
         </div>
     </div>
   );
