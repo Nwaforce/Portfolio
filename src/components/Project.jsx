3c819d7c6eca7c3c03502a3f6ed90469cc7ecb1e
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-01-16 16:02:17 +0100

    Project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import {PROJECTS} from "../constants"
 const Project = () => {
   return (
-    <div>
-        Projects
+    <div className='border-b border-neutral-900 pb-4'>
+      <h1 className='my-20 text-center text-4xl'>Projects</h1>  
+      <div>
+        {PROJECTS}
+      </div>
     </div>
   )
 }
