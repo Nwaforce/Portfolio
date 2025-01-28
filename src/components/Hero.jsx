5bcd180f068f0d5ca64e3864feea8a2e8779bdb3
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-01-28 10:22:15 +0100

    framer-motion

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/Manny.jpeg";
+import { motion } from "motion/react";
 
 const Hero = () => {
   return (
@@ -8,9 +9,15 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16">
+            <motion.h1
+              animate={{
+                x: 100,
+                transition: { duration: 2 },
+              }}
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16"
+            >
               Nwafor Emmanuel
-            </h1>
+            </motion.h1>
             <span
               className="bg-gradient-to-r from-pink-300 via-slate-500 
                 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
