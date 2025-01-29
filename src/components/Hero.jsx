f0c8df32d82a1526299931dff9a73a13350c041d
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-01-29 21:05:05 +0100

    transition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,21 +9,19 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <motion.h1
-              animate={{
-                x: 100,
-                transition: { duration: 2 },
-              }}
+            <motion.h1 initial={{x : -100, opacity : 0}}
+            animate={{x : 0, opacity : 1}}
+            transition={{duration : 0.5, delay : 0}}
               className="pb-16 text-6xl font-thin tracking-tight lg:mt-16"
             >
               Nwafor Emmanuel
             </motion.h1>
-            <span
+            <motion.span
               className="bg-gradient-to-r from-pink-300 via-slate-500 
                 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
               Fontend Developer
-            </span>
+            </motion.span>
             <p className="my-2 max-w-xl py-6 font-light tracking-tight">
               {HERO_CONTENT}
             </p>
