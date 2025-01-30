98fe364324a87c40502e266dae10e28cc075a85e
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-01-30 10:48:55 +0100

    transition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,16 @@ import React from "react";
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/Manny.jpeg";
 import { motion } from "motion/react";
+import { delay } from "framer-motion";
+
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: 0.5 },
+  },
+});
 
 const Hero = () => {
   return (
@@ -9,22 +19,31 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <motion.h1 initial={{x : -100, opacity : 0}}
-            animate={{x : 0, opacity : 1}}
-            transition={{duration : 0.5, delay : 0}}
+            <motion.h1
+              variants={container(0)}
+              initial={hidden}
+              animate={visible}
               className="pb-16 text-6xl font-thin tracking-tight lg:mt-16"
             >
               Nwafor Emmanuel
             </motion.h1>
             <motion.span
+              variants={container(0.5)}
+              initial={hidden}
+              animate={visible}
               className="bg-gradient-to-r from-pink-300 via-slate-500 
                 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
               Fontend Developer
             </motion.span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tight">
+            <motion.p
+              variants={container(1)}
+              initial={hidden}
+              animate={visible}
+              className="my-2 max-w-xl py-6 font-light tracking-tight"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
