7c10339827ac24c85f774558c06173568003afb6
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-03-17 20:05:40 +0100

    resent

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 // import project1 from "../assets/projects/project-1.jpg";
 // import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+// import project3 from "../assets/projects/project-3.jpg";
+// import project4 from "../assets/projects/project-4.jpg";
 import project0 from "../assets/projects/project-2.png";
 import kalmly from "../assets/projects/kalmly.png";
 import manny from "../assets/projects/manny.png";
