1fe7e240cf75146dbcad3e8c77659377fcfff0c6
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-04-15 15:01:19 +0100

    hel

--- src/constants/index.js
+++ src/constants/index.js
@@ -93,13 +93,13 @@ export const PROJECTS = [
     link: "https://what-bytes-task-emma.vercel.app/",
   },
 
-  //   {
-  //     title: "Blogging Platform",
-  //     image: project4,
-  //     description:
-  //       "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-  //     technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
-  //   },
+    {
+      title: "Blogging Platform",
+      image: project4,
+      description:
+        "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+      technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+    },
 ];
 
 export const CONTACT = {
