3426a154ae51a9dae83a112229f83eab2997a640
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2025-04-22 13:15:26 +0100

    h

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         {/* <img src={logo} alt="logo" /> */}
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        {/* <FaGithub /> */}
+
         <a
           href="https://github.com/Nwaforce"
           target="_blank"
@@ -17,8 +17,8 @@ const NavBar = () => {
         >
           <FaGithub />
         </a>
-        {/* <FaLinkedin /> */}
-        <a
+ 
+         <a
           href="https://www.linkedin.com/in/nwafor-emmanuel-b82733217/"
           target="_blank"
           rel="noopener noreferrer"
